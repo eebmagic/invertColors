93004fbb468ac894d604557dee32eddc1e7d7f2d
Ethan B <eebmagic@gmail.com>
2019-08-13 18:01:49 -0400

    small style changes

--- invert_colors.py
+++ invert_colors.py
@@ -1,24 +1,25 @@
 from PIL import Image
 
+
 def makeOutputPath(inputPath):
 	SPLITS = inputPath.split('/')
 	container = '/'.join(SPLITS[:-1])
 	return container + "/output.png"
 
-### Load Image Data ###
+
+# Load Image Data #
 image_PATH = input("Drag Image Here: ").strip()
 im = Image.open(image_PATH)
 raw_data = list(im.getdata())
 
-
-### Invert Color Data ###
+# Invert Color Data #
 new_data = []
 for point in raw_data:
 	one, two, three = point[:3]
 	newPoint = (255 - one, 255 - two, 255 - three)
 	new_data.append(newPoint)
 
-### Output New Image Data ###
+# Output New Image Data #
 newIm = Image.new("RGB", im.size)
 newIm.putdata(new_data)
 new_PATH = makeOutputPath(image_PATH)
